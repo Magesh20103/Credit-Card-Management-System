5213d35f49461646d98618f5a488747bd5b9ba37
Magesh <131270877+Magesh20103@users.noreply.github.com>
2024-04-08 15:26:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask,render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 from datetime import datetime
-from flask import flash
 
 app = Flask(__name__)
 app.secret_key = '' 
